3a51c6bc489c2f0b2361a3327ec3af2df4c0703e
GusFurtado <gustavofurtado2@gmail.com>
2024-06-18 21:03:25 -0300

    🚚 Renaming package to `banana`

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="Cadastro",
+    name="banana",
     version="0.0.1",
     author="Gustavo Furtado",
     author_email="gustavofurtado2@gmail.com",
