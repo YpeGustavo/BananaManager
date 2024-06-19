77392630eacb8d269cc4de215f9001bde152e674
GusFurtado <gustavofurtado2@gmail.com>
2024-06-19 07:10:57 -0300

    ⬇️ Replace pandas for sqlalchemy solution

--- banana/callbacks/load_table.py
+++ banana/callbacks/load_table.py
@@ -1,8 +1,11 @@
 from dash import html, Output, Input, callback
-import pandas as pd
-from sqlalchemy import create_engine
+from sqlalchemy import MetaData, Table, create_engine, select
 
-from ..configs import CONFIG
+from ..config import CONFIG
+from ..models import TABLES
+
+
+metadata = MetaData()
 
 
 @callback(
@@ -12,13 +15,35 @@ from ..configs import CONFIG
     prevent_initial_call=True,
 )
 def load_table(tablename: str):
-    query = f"select * from {tablename}"
+    # Get table model
+    table = next(table for table in TABLES if table.name == tablename)
 
+    # Get table schema
     engine = create_engine(CONFIG.connection_string)
+    tabledata = Table(tablename, metadata, autoload_with=engine)
+
+    # Create select statement
+    if table.columns is None:
+        query = select(tabledata)
+    else:
+        query = select(
+            *[
+                getattr(tabledata.c, col.name).label(col.pretty_name)
+                for col in table.columns
+            ]
+        ).select_from(tabledata)
+
+    # Print datatypes
+    print([(col.name, str(col.type)) for col in tabledata.columns])
+
+    # Get data
     with engine.connect() as conn:
-        df = pd.read_sql(query, con=conn)
+        result = conn.execute(query)
+        columns = result.keys()
 
-    thead = html.Tr([html.Th(col) for col in df.columns])
-    tbody = [html.Tr([html.Td(td) for td in row]) for _, row in df.iterrows()]
-
-    return [thead, tbody]
+        # Build HTML
+        thead = html.Tr([html.Th(col) for col in columns])
+        tbody = [
+            html.Tr([html.Td(value) for value in row]) for row in result.fetchall()
+        ]
+        return [thead, tbody]
