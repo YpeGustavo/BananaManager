29379a72acc177f121c54603441aa3e5418f8fe6
YpeGustavo <gustavo.silva@ype.ind.br>
2024-12-06 15:52:17 -0300

    ✨ `queries.get_primary_key`

--- banana/queries/__init__.py
+++ banana/queries/__init__.py
@@ -1,5 +1,6 @@
 from .create_foreign_key_options import create_foreign_key_options
 from .execute_query import exec_sql, read_sql
 from .delete_row import delete_row
+from .get_primary_key import get_primary_key
 from .insert_row import insert_row
 from .update_cell import update_cell
---
+++ banana/queries/get_primary_key.py
@@ -0,0 +1,47 @@
+from sqlalchemy import Table, MetaData
+
+from ..core.instances import db
+from ..models.table_models import BananaTable
+
+
+def get_primary_key(banana_table: BananaTable) -> str:
+    """
+    Check if a table has a primary key, ensure it has only one column, and return the column name.
+
+    Args:
+        table_name (str): Name of the table to check.
+
+    Returns:
+        str: The name of the primary key column.
+
+    Raises:
+        ValueError: If the table has no primary key or if the primary key consists of multiple columns.
+
+    Example:
+        get_primary_key_column('users')
+        # Output: 'id'
+    """
+
+    metadata = MetaData(bind=db.engine)
+    table = Table(
+        banana_table.name,
+        metadata,
+        schema=banana_table.schema_name,
+        autoload_with=db.engine,
+    )
+
+    # Get the primary key columns
+    primary_key_columns = list(table.primary_key.columns)
+
+    if not primary_key_columns:
+        raise ValueError(
+            f"The table '{banana_table.name}' does not have a primary key."
+        )
+
+    if len(primary_key_columns) > 1:
+        raise ValueError(
+            f"The table '{banana_table.name}' has a composite primary key: {', '.join(col.name for col in primary_key_columns)}"
+        )
+
+    # Return the single primary key column name
+    return primary_key_columns[0].name
