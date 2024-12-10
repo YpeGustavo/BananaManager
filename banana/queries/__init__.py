23633d4407326501b5a21673dc7b174037ab52a9
YpeGustavo <gustavo.silva@ype.ind.br>
2024-12-10 10:49:00 -0300

    🚚 Disambiguate `queries` file names

--- banana/queries/__init__.py
+++ banana/queries/__init__.py
@@ -1,7 +1,7 @@
-from .create_foreign_key_options import create_foreign_key_options
-from .execute_query import exec_sql, read_sql
-from .delete_row import delete_row
-from .get_primary_key import get_primary_key
-from .insert_row import insert_row
-from .select_table import select_table
-from .update_cell import update_cell
+from ._create_foreign_key_options import create_foreign_key_options
+from ._execute_query import exec_sql, read_sql
+from ._delete_row import delete_row
+from ._get_primary_key import get_primary_key
+from ._insert_row import insert_row
+from ._select_table import select_table
+from ._update_cell import update_cell
