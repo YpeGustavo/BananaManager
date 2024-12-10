2157cb558eff0725a417dd47d1be7345d8091847
YpeGustavo <gustavo.silva@ype.ind.br>
2024-12-10 12:56:10 -0300

    ♻️ `update_cell`

--- banana/callbacks/_update_cell.py
+++ banana/callbacks/_update_cell.py
@@ -4,78 +4,48 @@ from ..core.config import config, db
 from ..core.history import LogType, post_history
 from ..core.tables import tables
 from ..core.utils import raise_error, split_pathname
+from ..queries import update_cell
 
 
 class UpdateCellCallback:
     def __init__(self, data: list[dict[str, str]], pathname: str):
         assert len(data) == 1, data
 
+        # Get table name
+        self.group_name, table_name = split_pathname(pathname)
+        self.banana_table = tables(self.group_name, table_name)
+
+        # Extract values from Ag Grid click data
         self.col_id = data[0]["colId"]
         self.row_id = data[0]["rowId"]
         self.old_value = data[0]["oldValue"]
         self.new_value = data[0]["value"]
 
-        self.metadata = MetaData()
-        self.group_name, table_name = split_pathname(pathname)
-        self.banana_table = tables(self.group_name, table_name)
+        # Inverse values from labels
+        banana_column = self.banana_table.get_column_by_name(self.col_id)
+        if banana_column.dataType.type in ("foreign", "enumerator"):
+            self.old_value = banana_column.data.inv[self.old_value]
+            self.new_value = banana_column.data.inv[self.new_value]
 
     def exec(self):
-        table_data = Table(
-            self.banana_table.name,
-            self.metadata,
-            schema=self.banana_table.schema_name,
-            autoload_with=db.engine,
+        update_cell(
+            table_name=self.banana_table.name,
+            schema_name=self.banana_table.schema_name,
+            row_id=self.row_id,
+            column=self.col_id,
+            new_value=self.new_value,
         )
 
-        banana_column = next(
-            col for col in self.banana_table.columns if col.name == self.col_id
+        post_history(
+            log_type=LogType.UPDATE,
+            group_name=self.group_name,
+            table_name=self.banana_table.name,
+            schema_name=self.banana_table.schema_name,
+            user_name=config.connection.username,
+            log_data={
+                "column_name": self.col_id,
+                "row_id": self.row_id,
+                "old_value": self.old_value,
+                "new_value": self.new_value,
+            },
         )
-
-        if banana_column.dataType.type == "foreign":
-            foreign_table = Table(
-                banana_column.dataType.data["tableName"],
-                self.metadata,
-                schema=banana_column.dataType.data["schemaName"],
-                autoload_with=db.engine,
-            )
-
-            with db.engine.connect() as conn:
-                id_col = foreign_table.c[banana_column.dataType.data["columnName"]]
-                label = foreign_table.c[banana_column.dataType.data["columnDisplay"]]
-
-                query = (
-                    select(id_col)
-                    .select_from(foreign_table)
-                    .where(label == self.new_value)
-                )
-
-                result = conn.execute(query)
-                rows = result.fetchall()
-                self.new_value = rows[0][0]
-
-        try:
-            with db.engine.connect() as conn:
-                query = (
-                    update(table_data)
-                    .where(table_data.c[self.banana_table.primary_key] == self.row_id)
-                    .values({self.col_id: self.new_value})
-                )
-                conn.execute(query)
-                conn.commit()
-
-                post_history(
-                    log_type=LogType.UPDATE,
-                    group_name=self.group_name,
-                    table_name=self.banana_table.name,
-                    schema_name=self.banana_table.schema_name,
-                    user_name=config.connection.username,
-                    log_data={
-                        "column_name": self.col_id,
-                        "row_id": self.row_id,
-                        "old_value": self.old_value,
-                        "new_value": self.new_value,
-                    },
-                )
-
-        except Exception as e:
-            raise_error("Error updating cell", str(e.orig))
